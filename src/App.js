e693fb0785050936f1bf7c859537deb5971e2cc7
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2018-05-31 14:23:50 +0300

    navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import pubsub from 'pubsub-js';
 import Button from './components/button.component';
 import Input from './components/input.component';
 
+const screens = {
+  intro: Intro,
+  creator: Creator,
+  publish: Publish,
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentScreen: 'intro'
+    };
+
+    pubsub.subscribe('navigate', ({ screen }) => {
+      this.setState({ currentScreen: screen });
+    });
+  }
+
   render() {
+    const Screen = screens[this.state.currentScreen];
+
     return (
       <div className="App">
-        <Input placeholder="Title" inputId="story-title"/>
-        <Button>Create</Button>
+        <Screen />
       </div>
     );
   }
