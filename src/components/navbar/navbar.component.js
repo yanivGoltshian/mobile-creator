888321642d82dc3115149f5e0aa0a48996c2eb70
Yaniv <yaniv@playbuzz.com>
2018-05-31 23:33:31 +0300

    nav bar

--- src/components/navbar/navbar.component.js
+++ src/components/navbar/navbar.component.js
@@ -9,17 +9,23 @@ export default class Navbar extends React.Component {
     render() {
         return (
             <div className="creator-header">
-                <span className="header-title">Story Create</span>
+                <div className="header-title">Story Create</div>
                 <div className="header-icons">
                     <ul className="header-icons-list">
                         <li>
-                            <button className="btn mdl-button mdl-js-button" onClick={this.props.onSave}>
-                                <i className="btn-icon material-icons">save</i>
+                            {/*<button className="btn mdl-button mdl-js-button" onClick={this.props.onSave}>*/}
+                                {/*<i className="btn-icon material-icons">save</i>*/}
+                            {/*</button>*/}
+                            <button type="button" className="btn btn-primary nav-btn pb-btn-blue" onClick={this.props.onSave}>
+                                <span className="button-text">Save</span>
                             </button>
                         </li>
                         <li>
-                            <button className="btn mdl-button mdl-js-button" onClick={this.props.onPublish}>
-                                <i className="btn-icon material-icons">publish</i>
+                            {/*<button className="btn mdl-button mdl-js-button" onClick={this.props.onPublish}>*/}
+                                {/*<i className="btn-icon material-icons">publish</i>*/}
+                            {/*</button>*/}
+                            <button type="button" className="btn btn-primary nab-btn pb-btn-green" onClick={this.props.onPublish}>
+                                <span className="button-text">Publish</span>
                             </button>
                         </li>
                     </ul>
