13eb9008bb8e7e644f3c0de88619176f74e642de
Tal <tal.kaptsan@playbuzz.com>
2018-05-31 21:12:14 +0300

    feat(): item scheme

---
+++ src/services/item.service.js
@@ -0,0 +1,146 @@
+export default class ItemService {
+    constructor() {
+
+    }
+
+    get() {
+        return {
+            "id": "0a9ee147-da14-46ff-bb97-c0e598029f2d",
+            "tags": [],
+            "thumbnail": {
+                "mediaType": "image",
+                "originalImageUrl": "https://img.playbuzz.com/image/upload/v1527790190/fai1zodaea9cohsrp0lp.jpg",
+                "url": "https://img.playbuzz.com/image/upload/c_crop/ar_100:53,c_crop/c_mfit,w_640/ar_420:315,c_crop/c_fill,w_420/v1527790190/fai1zodaea9cohsrp0lp.jpg",
+                "canvasObjects": {},
+                "width": 1500,
+                "height": 1001,
+                "isAnimated": false,
+                "selected": {
+                    "h": 1001,
+                    "w": 1500,
+                    "x": 0,
+                    "y": 0,
+                    "x2": 1500,
+                    "y2": 1001
+                },
+                "fileSize": 347449,
+                "credits": "Getty Images",
+                "gettyAssetId": "908333824",
+                "gettyAccount": "Playbuzz"
+            },
+            "socialThumbnail": {
+                "mediaType": "image",
+                "originalImageUrl": "https://img.playbuzz.com/image/upload/v1527790190/fai1zodaea9cohsrp0lp.jpg",
+                "url": "https://img.playbuzz.com/image/upload/c_crop/ar_100:53,c_crop/c_mfit,w_640/ar_420:221,c_crop/c_fill,w_420/v1527790190/fai1zodaea9cohsrp0lp.jpg",
+                "canvasObjects": {},
+                "width": 1500,
+                "height": 1001,
+                "isAnimated": false,
+                "selected": {
+                    "h": 1001,
+                    "w": 1500,
+                    "x": 0,
+                    "y": 0,
+                    "x2": 1500,
+                    "y2": 1001
+                },
+                "fileSize": 347449,
+                "credits": "Getty Images",
+                "gettyAssetId": "908333824",
+                "gettyAccount": "Playbuzz"
+            },
+            "locale": "en-US",
+            "status": "published",
+            "translation": {
+                "enabled": true
+            },
+            "sponsored": {
+                "enabled": false
+            },
+            "permission": {
+                "view": "public"
+            },
+            "formatId": "story",
+            "sections": [
+                [
+                    {
+                        "title": {
+                            "ops": [
+                                {
+                                    "insert": "\n"
+                                }
+                            ]
+                        },
+                        "list": {
+                            "type": "none",
+                            "backgroundColor": "#009CFF",
+                            "color": "#fff",
+                            "enableVoting": true,
+                            "enableDownVoting": false
+                        },
+                        "media": {
+                            "mediaType": "image",
+                            "originalImageUrl": "https://img.playbuzz.com/image/upload/v1527790190/fai1zodaea9cohsrp0lp.jpg",
+                            "url": "https://img.playbuzz.com/image/upload/c_crop/v1527790190/fai1zodaea9cohsrp0lp.jpg",
+                            "canvasObjects": {},
+                            "width": 1500,
+                            "height": 1001,
+                            "isAnimated": false,
+                            "selected": {
+                                "h": 1001,
+                                "w": 1500,
+                                "x": 0,
+                                "y": 0,
+                                "x2": 1500,
+                                "y2": 1001
+                            },
+                            "fileSize": 347449,
+                            "credits": "Getty Images",
+                            "gettyAssetId": "908333824",
+                            "gettyAccount": "Playbuzz"
+                        },
+                        "id": "4ce622cb-c5f9-4b7a-8445-642fc75d42cc",
+                        "type": "mediaSection",
+                        "$$hashKey": "object:551",
+                        "settings": {
+                            "autoPlay": true
+                        },
+                        "description": {
+                            "ops": [
+                                {
+                                    "insert": "\n"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            ],
+            "cover": {
+                "mediaType": "image",
+                "originalImageUrl": "https://img.playbuzz.com/image/upload/v1527790190/fai1zodaea9cohsrp0lp.jpg",
+                "url": "https://img.playbuzz.com/image/upload/c_crop/ar_100:53,c_crop/c_mfit,w_640/v1527790190/fai1zodaea9cohsrp0lp.jpg",
+                "canvasObjects": {},
+                "width": 1500,
+                "height": 1001,
+                "isAnimated": false,
+                "selected": {
+                    "h": 1001,
+                    "w": 1500,
+                    "x": 0,
+                    "y": 0,
+                    "x2": 1500,
+                    "y2": 1001
+                },
+                "fileSize": 347449,
+                "credits": "Getty Images",
+                "gettyAssetId": "908333824",
+                "gettyAccount": "Playbuzz"
+            },
+            "readingTime": 4,
+            "title": "Story (2018-05-31 21:10:07)",
+            "detectedLocale": "en-US",
+            "channelId": "2da466c9-d201-45fc-a18d-5687aac968c5"
+        };
+    }
+
+}
