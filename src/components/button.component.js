538d2fe14abcbd7333204184c6ecca8d5f2f5287
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2018-05-31 12:55:52 +0300

    add material design

---
+++ src/components/button.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Button extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Clickme</button>
+        );
+    }
+}
